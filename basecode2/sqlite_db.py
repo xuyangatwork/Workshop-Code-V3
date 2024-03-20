937b75186c324879bc51276ad037e62b1c3424dc
Joe tay <tay.joe@gmail.com>
2024-03-20 10:05:34 +0800

    sql

--- basecode2/sqlite_db.py
+++ basecode2/sqlite_db.py
@@ -25,38 +25,40 @@ config_handler = ConfigHandler()
 SQL_DB = config_handler.get_value('DATABASE', 'SQL_DB')
 	
 def create_sql_db():
-    cwd = os.getcwd()
-    WORKING_DIRECTORY = os.path.join(cwd, "database")
-    WORKING_DATABASE = os.path.join(WORKING_DIRECTORY , SQL_DB)
-    
-    conn = sqlite3.connect(WORKING_DATABASE)
-    cursor = conn.cursor()
+	cwd = os.getcwd()
+	WORKING_DIRECTORY = os.path.join(cwd, "database")
+	if not os.path.exists(WORKING_DIRECTORY):
+		os.makedirs(WORKING_DIRECTORY)
+	WORKING_DATABASE = os.path.join(WORKING_DIRECTORY , SQL_DB)
+	
+	conn = sqlite3.connect(WORKING_DATABASE)
+	cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Data_Table (
-        data_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date TEXT,
-        user_id TEXT NOT NULL,
-        profile_id TEXT NOT NULL,
-        school_id TEXT NOT NULL,
-        chatbot_ans TEXT,
-        user_prompt TEXT,
-        function_name TEXT,
-        tokens INTEGER
-    )
-    
-    ''')
-
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Chatbot_Training_Records (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        chatbot_type TEXT NOT NULL,
-        chatbot_name TEXT NOT NULL,
-        prompt TEXT NOT NULL,
-        response TEXT NOT NULL,
-        user_id TEXT NOT NULL,
-        school_id TEXT NOT NULL
-    )
+	cursor.execute('''
+	CREATE TABLE IF NOT EXISTS Data_Table (
+		data_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT,
+		user_id TEXT NOT NULL,
+		profile_id TEXT NOT NULL,
+		school_id TEXT NOT NULL,
+		chatbot_ans TEXT,
+		user_prompt TEXT,
+		function_name TEXT,
+		tokens INTEGER
+	)
+	
 	''')
 
-    conn.commit()
+	cursor.execute('''
+	CREATE TABLE IF NOT EXISTS Chatbot_Training_Records (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		chatbot_type TEXT NOT NULL,
+		chatbot_name TEXT NOT NULL,
+		prompt TEXT NOT NULL,
+		response TEXT NOT NULL,
+		user_id TEXT NOT NULL,
+		school_id TEXT NOT NULL
+	)
+	''')
+
+	conn.commit()
